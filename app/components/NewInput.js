75192373ef55c1725a6e160a4fe399cec44c5646
吴凯 <744982965@qq.com>
2018-03-07 16:11:59 +0800

    修改内容：

--- app/components/NewInput.js
+++ app/components/NewInput.js
@@ -42,13 +42,13 @@ class NewInput extends React.Component {
         //规则库
         if(rule){
             if(rule.test == 'checkPassword'){
-                if(ruleFun[rule.passwodRule](text) && text == this.props.rule.password){
+                if(Util.ruleFun[rule.passwodRule](text) && text == this.props.rule.password){
                     status = 'pass'
                 }else{
                     status = 'fail'
                 }
             }else{
-                if(ruleFun[rule.test](text)){
+                if(Util.ruleFun[rule.test](text)){
                     status = 'pass'
                 }else{
                     status = 'fail'
@@ -164,27 +164,6 @@ class NewInput extends React.Component {
         this._checkRequired();
     }
 
-    componentWillReceiveProps(nextProps){
-        if(this.props.rule.test == 'checkPassword'){
-            let { rule, password1Text } = nextProps;
-            let status = '';
-            if(rule.password){
-                if(ruleFun[rule.passwodRule](password1Text) && password1Text == rule.password){
-                    status = 'pass'
-                }else{
-                    status = 'fail'
-                }
-
-                console.log(password1Text);
-
-                this.setState({
-                    status:status,
-                    text:password1Text
-                })
-            }
-        }
-    }
-
     render() {
         let {status} = this.state;
 
@@ -199,89 +178,6 @@ class NewInput extends React.Component {
     }
 }
 export default NewInput;
-
-let ruleFun={
-    'email':function (text) {
-        return /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/.test(text);
-    },
-    'phone':function (text) {
-        return /^13[0-9]{9}$|14[0-9]{9}|15[0-9]{9}$|18[0-9]{9}$/.test(text);
-    },
-    'idCard':function (gets) {
-        var reg = /^[^ ]$/; //不包含空格
-        //该方法由佚名网友提供;
-        var Wi = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2, 1]; // 加权因子;
-        var ValideCode = [1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2]; // 身份证验证位值，10代表X;
-
-        if (gets.length == 15) {
-            return isValidityBrithBy15IdCard(gets);
-        } else if (gets.length == 18) {
-            var a_idCard = gets.split(""); // 得到身份证数组
-            if (isValidityBrithBy18IdCard(gets) && isTrueValidateCodeBy18IdCard(a_idCard)) {
-                return true;
-            }
-            return false;
-        }
-        return false;
-
-        function isTrueValidateCodeBy18IdCard(a_idCard) {
-            var sum = 0; // 声明加权求和变量
-            if (a_idCard[17].toLowerCase() == 'x') {
-                a_idCard[17] = 10; // 将最后位为x的验证码替换为10方便后续操作
-            }
-            for (var i = 0; i < 17; i++) {
-                sum += Wi[i] * a_idCard[i]; // 加权求和
-            }
-            valCodePosition = sum % 11; // 得到验证码所位置
-            if (a_idCard[17] == ValideCode[valCodePosition]) {
-                return true;
-            }
-            return false;
-        }
-
-        function isValidityBrithBy18IdCard(idCard18) {
-            var year = idCard18.substring(6, 10);
-            var month = idCard18.substring(10, 12);
-            var day = idCard18.substring(12, 14);
-            var temp_date = new Date(year, parseFloat(month) - 1, parseFloat(day));
-            // 这里用getFullYear()获取年份，避免千年虫问题
-            if (temp_date.getFullYear() != parseFloat(year) || temp_date.getMonth() != parseFloat(month) - 1 || temp_date.getDate() != parseFloat(day)) {
-                return false;
-            }
-            return true;
-        }
-
-        function isValidityBrithBy15IdCard(idCard15) {
-            var year = idCard15.substring(6, 8);
-            var month = idCard15.substring(8, 10);
-            var day = idCard15.substring(10, 12);
-            var temp_date = new Date(year, parseFloat(month) - 1, parseFloat(day));
-            // 对于老身份证中的你年龄则不需考虑千年虫问题而使用getYear()方法
-            if (temp_date.getYear() != parseFloat(year) || temp_date.getMonth() != parseFloat(month) - 1 || temp_date.getDate() != parseFloat(day)) {
-                return false;
-            }
-            return true;
-        }
-    },
-    'n':function (text) {
-        return /^\d+$/.test(text);
-    },
-    'z2-4':function (text) {
-        return /^[\u4E00-\u9FA5\uf900-\ufa2d]{2,4}$/.test(text);
-    },
-    's':function (text) {
-        return /^[\u4E00-\u9FA5\uf900-\ufa2d\w\.\s]+$/.test(text);
-    },
-    's2-20':function (text) {
-        return /^[\u4E00-\u9FA5\uf900-\ufa2d\w\.\s]{2,20}$/.test(text);
-    },
-    's6-20':function (text) {
-        return /^[\u4E00-\u9FA5\uf900-\ufa2d\w\.\s]{6,20}$/.test(text);
-    },
-    '*':function (text) {
-        return /[\w\W]+/.test(text);
-    }
-}
 
 const styles = {
     newInput: {
